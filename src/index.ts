3d92161a3fb72f6a7ebf5348aedf27cd769e562c
startx88 <arya.creativemind@gmail.com>
2022-05-14 10:34:32 +0530

    add swagger in api

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,16 @@ import dotenv from "dotenv";
 dotenv.config({ path: `.env.${process.env.NODE_ENV ?? "development"}` });
 import path from "path";
 import express from "express";
+import YAML from "yamljs";
+import swagggerUI from "swagger-ui-express";
+import moragn from "morgan";
+import cors from "cors";
 import { connectDb } from "./db";
 import { userRoute } from "./api/routes/user";
 import { errorHandler } from "./api/middleware/error-handler";
+
+const sPath = path.resolve(__dirname, "./swagger.yaml");
+const jsDoc = YAML.load(sPath);
 
 // app
 const app = express();
@@ -15,6 +22,9 @@ app.set("views", path.resolve(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(cors({ origin: "http://localhost:3000" }));
+
+app.use(moragn("dev"));
 
 // routes
 app.get("/api", (req, res, next) => {
@@ -22,7 +32,7 @@ app.get("/api", (req, res, next) => {
 });
 
 app.use("/api/auth", userRoute);
-
+app.use("/api-docs", swagggerUI.serve, swagggerUI.setup(jsDoc));
 // error handler
 app.use(errorHandler);
 // server
